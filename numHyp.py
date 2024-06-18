ec8bc2fe3d254ddc89e6f96691882e4e685adc8c
Furryyy18 <nfury2980@gmail.com>
2024-06-18 10:16:15 +0530

    Create numHyp.py

---
+++ numHyp.py
@@ -0,0 +1,7 @@
+n = int(input("Enter the number:"))
+if n < 0 :
+  print("The number is negative")
+elif n > 0 :
+  print("The number is positive")
+else :
+  print("The number is neither negative nor positive")
